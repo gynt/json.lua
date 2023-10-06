b0621b0e16c3320fec76f2f288543c0e39b6b9d4
gynt <gynt@users.noreply.github.com>
2023-10-06 13:37:52 +0200

    Tweak encoding by setting functions and cycles to null

--- json.lua
+++ json.lua
@@ -61,34 +61,42 @@ local function encode_table(val, stack)
   stack = stack or {}
 
   -- Circular reference?
-  if stack[val] then error("circular reference") end
+  if stack[val] then 
+	
+	-- error("circular reference") 
+	return "null"
+  end
 
   stack[val] = true
 
-  if rawget(val, 1) ~= nil or next(val) == nil then
-    -- Treat as array -- check keys are valid and it is not sparse
-    local n = 0
-    for k in pairs(val) do
-      if type(k) ~= "number" then
-        error("invalid table: mixed or invalid key types")
-      end
-      n = n + 1
-    end
-    if n ~= #val then
-      error("invalid table: sparse array")
-    end
-    -- Encode
-    for i, v in ipairs(val) do
-      table.insert(res, encode(v, stack))
-    end
-    stack[val] = nil
-    return "[" .. table.concat(res, ",") .. "]"
+  -- if rawget(val, 1) ~= nil or next(val) == nil then
+    -- -- Treat as array -- check keys are valid and it is not sparse
+    -- local n = 0
+    -- for k in pairs(val) do
+      -- if type(k) ~= "number" then
+        -- error("invalid table: mixed or invalid key types")
+      -- end
+      -- n = n + 1
+    -- end
+    -- if n ~= #val then
+      -- error("invalid table: sparse array")
+    -- end
+    -- -- Encode
+    -- for i, v in ipairs(val) do
+      -- table.insert(res, encode(v, stack))
+    -- end
+    -- stack[val] = nil
+    -- return "[" .. table.concat(res, ",") .. "]"
 
-  else
+  -- else
+  if true then
     -- Treat as an object
     for k, v in pairs(val) do
       if type(k) ~= "string" then
-        error("invalid table: mixed or invalid key types")
+		k = tostring(k)
+		if k == nil then
+			error("invalid table: mixed or invalid key types")
+		end
       end
       table.insert(res, encode(k, stack) .. ":" .. encode(v, stack))
     end
